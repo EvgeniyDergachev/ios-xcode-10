6bb3b83f5f490a3876e5d70594a2c36039068028
Evgeniy Dergachev <v-evderg@microsoft.com>
2018-06-05 19:56:24 +0100

    Init repo

---
+++ AwesomeProject/RandomGenerator.swift
@@ -0,0 +1,15 @@
+//
+//  FeedbackGenerator.swift
+//  AwesomeProject
+//
+//  Created by user on 6/5/18.
+//  Copyright © 2018 user. All rights reserved.
+//
+
+import Foundation
+
+class FeedbackGenerator {
+    public func GenerateFeedback() -> String {
+        
+    }
+}
