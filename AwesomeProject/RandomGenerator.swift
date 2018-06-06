69dd1f40700a0158f358cf5a31a44c3f8a79551e
Evgeniy Dergachev <v-evderg@microsoft.com>
2018-06-06 19:24:35 +0100

    Shared scheme

--- AwesomeProject/RandomGenerator.swift
+++ AwesomeProject/RandomGenerator.swift
@@ -1,5 +1,5 @@
 //
-//  FeedbackGenerator.swift
+//  RandomGenerator.swift
 //  AwesomeProject
 //
 //  Created by user on 6/5/18.
@@ -8,8 +8,19 @@
 
 import Foundation
 
-class FeedbackGenerator {
-    public func GenerateFeedback() -> String {
+class RandomGenerator {
+    public func GenerateFeedback(length: Int) -> String {
+        let letters : NSString = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+        let len = UInt32(letters.length)
         
+        var randomString = ""
+        
+        for _ in 0 ..< length {
+            let rand = arc4random_uniform(len)
+            var nextChar = letters.character(at: Int(rand))
+            randomString += NSString(characters: &nextChar, length: 1) as String
+        }
+        
+        return randomString
     }
 }
--- AwesomeProjectTests/RandomGeneratorUnitTests.swift
+++ AwesomeProjectTests/RandomGeneratorUnitTests.swift
@@ -7,27 +7,34 @@
 //
 
 import XCTest
+@testable import AwesomeProject
 
 class RandomGeneratorUnitTests: XCTestCase {
-
+    
     override func setUp() {
+        super.setUp()
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
-
+    
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
     }
-
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    
+    func testRandomStringLenght() {
+        let length = 10;
+        let rndGenerator = RandomGenerator();
+        
+        let randomString = rndGenerator.GenerateFeedback(length: length);
+        
+        XCTAssert(randomString.count == length);
     }
-
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    
+    func testRandomStringIsNotEmpty() {
+        let rndGenerator = RandomGenerator();
+        let str = rndGenerator.GenerateFeedback(length: 5);
+        
+        XCTAssert(str.count != 0);
     }
 
 }
